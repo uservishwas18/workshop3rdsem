c0d11ee4b121583ac54f8a5d77b35e4ee3d15318
uservishwas18 <chaurasiyavishwas15@gmail.com>
2026-09-01 12:01:13 +0530

    index

---
+++ index.js
@@ -0,0 +1,24 @@
+// var a=10;
+// var b=20;
+// var c=30;
+// console.log(a+b+c);
+
+//  var x=5;
+//  if (x==5){
+    //  console.log("x is 5");
+//  }
+//  else{
+    //  console.log("x is not 5");
+// }
+
+
+// for(var i=0; i<5; i++){
+    // console.log(i);
+// }
+
+var x=20;
+const arr=[1,2,3,4,5];
+for (var i=0; i<arr.length; i++){
+    console.log(arr[i]);
+    console.log(arr[2]);
+}
