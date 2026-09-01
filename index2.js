7a33125a5ea6c95542ac27f82b0550704e86c9cf
uservishwas18 <chaurasiyavishwas15@gmail.com>
2026-09-01 12:02:04 +0530

    index2

---
+++ index2.js
@@ -0,0 +1,84 @@
+// var a = "Something";
+// console.log(a);
+
+// let text = "Something";
+// console.log(text);
+
+// console.log("\n==== If else ====");
+
+// let a = 30;
+// let b = 20;
+
+// if (a > b) {
+//     console.log("a is greater than b");
+// } else {
+//     console.log("a is not greater than b");
+// }
+
+// console.log("\n==== function with parameters ====");
+
+// function greet(fname,lname) {
+
+//     console.log("Hello",fname,lname);
+// }
+
+// greet("John","Doe");
+// greet("Jane","Smith");
+
+// console.log("\n==== function Return ====");
+
+// function add(a,b) {
+
+//     return a + b;
+// }
+
+// let result = add(10,20);
+// console.log("Result:",result);
+
+//percentage calculation
+// function marks(a,b){
+//     return (((a+b)/200)*100);
+// }
+// console.log(marks(80,90),"%");
+// //while loop
+
+// console.log("\n==== While Loop ====");
+
+// let count = 1;
+
+// while (count <= 5) {
+
+//     console.log(count);
+
+//     count++;
+// }
+
+// let number = 1;
+// do{
+//     console.log("Node.js class");
+//     number++;
+// } while (number <= 5);
+// let a = new Array( );
+// a = [10,20,30,40,50];
+// let sum = 0;
+// for(let index = 0;index < arr.length;index++){
+ 
+//     console.log(arr[index]);
+//     sum += arr[index];
+// }
+
+// console.log("\n==== 2D Array ====");
+
+// let students = [
+
+//     ["Vishwas", 20, "A"],
+//     ["Zayn", 22, "B"],
+//     ["Heisenberg", 21, "C"]
+// ];
+
+// for(let row = 0; row < students.length; row++){
+//     for(let col = 0; col < students[row].length; col++){
+//         console.log([students[row]],[students[col]]);
+//     }
+// }
+
